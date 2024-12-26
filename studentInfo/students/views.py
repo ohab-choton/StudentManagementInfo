f4b1c0c4f9330e95efc53968d097d60ae47b1cf6
Abdul Ohab <ohabchoton@gmail.com>
2024-12-26 19:08:25 +0600

    till modelview

--- studentInfo/students/views.py
+++ studentInfo/students/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Student
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'students/index.html' ,{'students': Student.objects.all()})
+
+def view_student(request, id):
+    student = get_object_or_404(Student, id=id)
+    return render(request, 'students/view_student.html', {'student': student})
+
