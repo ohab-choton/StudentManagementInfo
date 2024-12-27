400aa17460025e78724f4243e4ae38961a68a746
Abdul Ohab <ohabchoton@gmail.com>
2024-12-28 01:28:09 +0600

    add delete button

--- studentInfo/students/views.py
+++ studentInfo/students/views.py
@@ -14,25 +14,6 @@ def view_student(request, id):
     student = get_object_or_404(Student, id=id)
     return render(request, 'students/view_student.html', {'student': student})
 
-# def add(request):
-#     if request.method=="POST":
-#         form=StudentForm(request.POST)
-#         if form.is_valid():
-#             new_student_number=form.cleaned_data['student_number']
-#             new_first_name=form.cleaned_data['first_name']
-#             new_last_name=form.cleaned_data['last_name']
-#             new_email=form.cleaned_data['email']
-#             new_field_of_study=form.cleaned_data['field_of_study']
-#             new_gpa=form.cleaned_data['gpa']
-
-#             new_student=Student( student_number=new_student_number, first_name=new_first_name, last_name=new_last_name, email=new_email, field_of_study=new_field_of_study, gpa=new_gpa)
-#             new_student.save()
-#             return render(request,'students/add.html',{'form':StudentForm(),'success':True } )
-
-#     else:
-#         form=StudentForm()
-#     return render(request,'students/add.html',{'form':StudentForm()})
-
 def add(request):
     if request.method == "POST":
         form = StudentForm(request.POST)
@@ -76,6 +57,11 @@ def edit(request, id):
         # Handle GET request: render the form with the existing student data
         form = StudentForm(instance=student)
         return render(request, 'students/edit.html', {'form': form, 'id': id})
+
+def delete(request, id):
+    student = get_object_or_404(Student, id=id)
+    student.delete()
+    return HttpResponseRedirect(reverse('index'))
 
 
 
